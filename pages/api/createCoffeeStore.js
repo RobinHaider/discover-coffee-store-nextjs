cf5c8297c4a4c1fee852027c8cd2bcb8c4098443
Robin Haider <robinhaider69@gmail.com>
2022-07-23 08:45:11 +0600

    airtable lib and createCoffeeStore api

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,12 +1,54 @@
-const Airtable = require('airtable');
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  process.env.AIRTABLE_BASE_KEY
-);
+import {
+  table,
+  getMinifiedRecords,
+  findRecordByFilter,
+} from '../../lib/airtable';
 
-const table = base('coffee-stores');
+const createCoffeeStore = async (req, res) => {
+  if (req.method === 'POST') {
+    //find a record
 
-const createCoffeeStore = (req, res) => {
-    res.json({message: 'Hello, folks!'})
-}
+    const { id, name, neighbourhood, address, imgUrl, voting } = req.body;
+
+    try {
+      if (id) {
+        const records = await findRecordByFilter(id);
+
+        if (records.length !== 0) {
+          res.json(records);
+        } else {
+          //create a record
+          if (name) {
+            const createRecords = await table.create([
+              {
+                fields: {
+                  id,
+                  name,
+                  address,
+                  neighbourhood,
+                  voting,
+                  imgUrl,
+                },
+              },
+            ]);
+
+            const records = getMinifiedRecords(createRecords);
+            res.json(records);
+          } else {
+            res.status(400);
+            res.json({ message: 'Id or name is missing' });
+          }
+        }
+      } else {
+        res.status(400);
+        res.json({ message: 'Id is missing' });
+      }
+    } catch (err) {
+      console.error('Error creating or finding a store', err);
+      res.status(500);
+      res.json({ message: 'Error creating or finding a store', err });
+    }
+  }
+};
 
 export default createCoffeeStore;
